65570465afd2afe008f755df59822c1e013098b1
KitagawaKouta <kkitagawa16219@gmail.com>
2024-02-16 15:55:34 +0900

    model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractUser,
+    Group,
+)
 # Create your models here.
+
+class CustomUser(AbstractUser):
+  def _create_user(self,username,password=None,**extra_fields):
+    if not username:
+      raise ValueError('The username must be set')
+    user = self.model(username=username,**extra_fields)
+    user.set_password(password)
+    user.save(using=self._db)
+    return user
+
+class CustomUserManager(BaseUserManager):
+  def create_user(self,username,password=None,**extra_fields):
+    extra_fields.setdefault('is_staff',True)
+    extra_fields.setdefault('is_superuser',False)
+    user = self._create_user(username,password,**extra_fields)
+    is_student , created = Group.objects.get_or_create(name='student')
+    user.groups.add(is_student)
+    return  user
+
+  def create_superuser(self,username,password=None,**extra_fields):
+    extra_fields.setdefault('is_staff',True)
+    extra_fields.setdefault('is_superuser',True)
+    user = self._create_user(username,password,**extra_fields)
+    is_superuser , created = Group.objects.get_or_create(name='superuser')
+    user.groups.add(is_superuser)
+    return user
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib import admin
+from django.urls import path
+from .views import (
+    StudentRegisterView,
+    SuperUserRegisterView,
+    StudentLoginView,
+    SuperUserLoginView,
+    UserLogoutView,
+)
 
-# Create your views here.
+urlpatterns = [
+    path("student_register/", StudentRegisterView.as_view(), name="student_register"),
+    path("superuser_register/", SuperUserRegisterView.as_view(), name="superuser_register"),
+    path("student_login/", StudentLoginView.as_view(),name="student_login"),
+    path("superuser_login/", SuperUserLoginView.as_view(),name="superuser_login"),
+    path("logout/", UserLogoutView.as_view(),name="logout"),
+]
