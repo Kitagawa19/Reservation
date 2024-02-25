3e243c341ce468d2205fd4ca43e57cd316dae62b
KitagawaKouta <kkitagawa16219@gmail.com>
2024-02-26 01:18:26 +0900

    readme update

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="container">
     <Header />
-    <slot />
+    <NuxtPage />
+    <Footer />
   </div>
 </template>
+<style>
+.container {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  background-color: #e0ffff;
+}
+</style>
