251c26360bb420ac2fa13e3f5b5ffe3371a20b8a
KitagawaKouta <kkitagawa16219@gmail.com>
2024-02-26 00:30:44 +0900

    crete end_time

--- frontend/components/Booking.vue
+++ frontend/components/Booking.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 const room_number = ref('');
 const start_time = ref(null);
+const end_time = ref(null);
 const purpose = ref('');
 const times = ['9:00','9:30','10:00','10:30','11:00','11:30','12:00','12:30',
                 '13:00','13:30','14:00','14:30','15:00','15:30','16:00','16:30',
@@ -45,7 +46,13 @@ onMounted(async () => {
           <input type="date" class="form-control" id="exampleDate" name="date">
         </div>
         <div class="form-floating mb-3">
-          <label for="floatingInput" v-show="!start_time">開始時間</label>
+          <label for="floatingInput" v-show="!start_time">いつ頃からにしますか？</label>
+          <select class="form-select" aria-level="start_time" v-model="start_time">
+            <option v-for="time in times" :key="time" :value="time">{{ time }}</option>
+          </select> 
+        </div>
+        <div class="form-floating mb-3">
+          <label for="floatingInput" v-show="!start_time">いつまでいますか？</label>
           <select class="form-select" aria-level="start_time" v-model="start_time">
             <option v-for="time in times" :key="time" :value="time">{{ time }}</option>
           </select> 
