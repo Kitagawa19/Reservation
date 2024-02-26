e3a3d4d9dbb293f1b5e824fc3670a43227d98f22
KitagawaKouta <kkitagawa16219@gmail.com>
2024-02-26 21:13:14 +0900

    reservation complete

--- frontend/components/Booking.vue
+++ frontend/components/Booking.vue
@@ -10,7 +10,6 @@ const startTimes = ['9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00',
 const endTimes = ['9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30',
   '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30',
   '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00',];
-const message = ref('');
 
 let roomData = ref({
   rooms: [],
@@ -71,7 +70,7 @@ const apply = async () => {
 <template>
   <div class="backgroundSetting">
     <div class="container p-3">
-      <div class="card" v-if="!message.value">
+      <div class="card">
         <div class="card-header">
           予約表
         </div>
